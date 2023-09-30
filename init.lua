799ad4572e4c294ab4860ed77abaedbde5a24768
matvey-mayner <144268527+matvey-mayner@users.noreply.github.com>
2023-09-30 13:01:07 +0300

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,25 @@
+--XidoOS core
+local bootfs = component.proxy(computer.getBootAddress())
+local tmpfs = component.proxy(computer.tmpAddress())
+
+local function getFile(fs, path)
+    local file, err = fs.open(path, "rb")
+    if not file then return nil, err end
+
+    local buffer = ""
+    repeat
+        local data = fs.read(file, math.huge)
+        buffer = buffer .. (data or "")
+    until not data
+    fs.close(file)
+
+    return buffer
+end
+
+local bootfile = "/system/core/bootloader.lua"
+if tmpfs.exists("/bootTo") then
+    bootfile = assert(getFile(tmpfs, "/bootTo"))
+    tmpfs.remove("/bootTo")
+end
+
+assert(load(assert(getFile(bootfs, bootfile)), "=" .. bootfile, nil, _ENV))()
